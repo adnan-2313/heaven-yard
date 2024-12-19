ff9abfbebda32ae2ef1790a1742c30a2790b29c8
Adnan <ak567845@gmail.com>
2024-12-19 10:13:31 +0530

    project-zolvit initialize

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <>
+      <div className="bg-gradient-to-tr from-[#0f0f10] to-[#0f0f10]">
+        <Home />
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+const Header = () => {
+  return (
+    <header className="w-full max-w-7xl flex justify-between p-4  mx-auto">
+      <img
+        src="https://assets.zolvit.com/live-images/zolvit-logo-white.svg"
+        alt="zolvit"
+        className="w-36"
+      />
+      <nav className="flex justify-between items-center py-4">
+        
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
